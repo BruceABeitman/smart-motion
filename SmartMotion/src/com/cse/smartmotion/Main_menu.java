bf756561a7080283f4cd8abb998bf335172c73f1
marco.brok1985@gmail.com <marco.brok1985@gmail.com@c9c09509-a659-163b-3c0f-702be18c0448>
2014-02-26 16:04:15 +0000

    added activities

--- SmartMotion/src/com/cse/smartmotion/Main_menu.java
+++ SmartMotion/src/com/cse/smartmotion/Main_menu.java
@@ -1,35 +1,35 @@
 package com.cse.smartmotion;
 
+import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 
-// Here's a comment for you
-// I changed something here for you
-//what happend here???
-public class MainActivity extends Activity {
+public class Main_menu extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.main_menu_activity);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
 	}
+
 	
-	
-	public void Main_Menu(View view){
-		Intent intent=new Intent(this, Main_menu.class);
+	public void New_Gesture(View view){
+		Intent intent=new Intent(this, New_Gesture.class);
 		startActivity(intent);
 	}
 	
+	public void Settings(View view){
+		Intent intent=new Intent(this, Settings.class);
+		startActivity(intent);
+	}
 	
-
 }
