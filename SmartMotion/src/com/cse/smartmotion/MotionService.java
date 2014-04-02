3c88fbd64ab6b54846b562cebe413bee853b1d9f
indelcecio@gmail.com <indelcecio@gmail.com@c9c09509-a659-163b-3c0f-702be18c0448>
2014-04-02 18:43:08 +0000

    Made some start to getting Service to run, ran into a few issues.

--- SmartMotion/src/com/cse/smartmotion/MotionService.java
+++ SmartMotion/src/com/cse/smartmotion/MotionService.java
@@ -1,10 +1,16 @@
 package com.cse.smartmotion;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.TriggerEvent;
+import android.hardware.TriggerEventListener;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -15,7 +21,14 @@ import android.util.Log;
  *
  */
 public class MotionService extends Service {
- /**
+	
+	private SensorManager mSensorManager;
+	private Sensor mSensor;
+	private TriggerEventListener mTriggerEventListener;
+	
+	private SensorManager sensorManager;
+	
+	/**
   * Delay until first execution of the Log task.
   */
  private final long mDelay = 0;
@@ -53,12 +66,39 @@ public class MotionService extends Service {
   Log.i(LOGTAG, "created");
   mTimer = new Timer();
   mLogTask = new LogTask();
+ 
  }
  
  @Override
  public void onStart(final Intent intent, final int startId) {
   super.onStart(intent, startId);
-  Log.i(LOGTAG, "started");
-  mTimer.schedule(mLogTask, mDelay, mPeriod);
+  
+  mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+  mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_SIGNIFICANT_MOTION);
+  
+  mTriggerEventListener = new TriggerEventListener() {
+	    @Override
+	    public void onTrigger(TriggerEvent event) {
+//	    	RawData gyroData = new RawData();
+//	    	ArrayList sensorData=new ArrayList();
+//	    	StringBuilder record=new StringBuilder();
+////    		started=true;
+//	    	SensorManager sensorManager;
+//	    	sensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
+//    		Sensor accel=sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+////    		this.startTime = System.currentTimeMillis();
+////    		this.lastTimePatternChecked = this.startTime;
+//    		sensorManager.registerListener(this, accel,SensorManager.SENSOR_DELAY_FASTEST);
+////    		requestTriggerSensor(TriggerEventListener, Sensor)
+	    	Log.i(LOGTAG, "Significant Motion Detected!!");
+	    }
+	};
+	
+  mSensorManager.requestTriggerSensor(mTriggerEventListener, mSensor);
+  
+  // Used to verify service running
+  //  Log.i(LOGTAG, "started");
+  //  mTimer.schedule(mLogTask, mDelay, mPeriod);
+  
  }
 }
